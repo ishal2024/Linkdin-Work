f6859bfb21841665e94ad1a58986a301d2a4c020
Vishal Jha <jhav8422@gmail.com>
2025-08-03 15:25:38 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -26,6 +26,19 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
+app.options('*', cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 //Router Imports
 const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/post.routes')
