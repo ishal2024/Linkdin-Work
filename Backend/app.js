08aa9f9dff80285242f6aee464c07e6d57e66de2
Vishal Jha <jhav8422@gmail.com>
2025-08-03 15:04:01 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -11,23 +11,20 @@ app.use(express.json())
 app.use(cookieparser())
 app.use(express.urlencoded({extended : true}))
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://linkdinass.netlify.app"
-];
+const allowedOrigins = ['http://localhost:5173', 'https://linkdinass.netlify.app']
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 //Router Imports
 const userRouter = require('./routes/user.routes')
